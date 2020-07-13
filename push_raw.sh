fd521517f2cd438edd8fdb183bddaed2c144ece1
Hannes Datta <h.datta@tilburguniversity.edu>
2020-07-13 09:40:27 +0200

    put api key in external file (api key is still invalid) - do not version this file

--- push_raw.sh
+++ push_raw.sh
@@ -1,1 +1,9 @@
-java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=b1249a40-25af-4c72-815e-fbcfeba19839 -did=doi:10.34894/DX857S -recurse rawdata-confidential
+. credentials.txt
+while true; do
+    read -p "You are about to store the raw data publicly. Remember to make such data confidential. Do you want to continue? Type yes or no." yn
+    case $yn in
+        [Yy]* ) java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=$dataverse_key -did=doi:10.34894/DX857S -recurse rawdata-confidential; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
--- push_release.sh
+++ push_release.sh
@@ -1,2 +1,9 @@
-java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=b1249a40-25af-4c72-815e-fbcfeba19839 -did=doi:10.34894/DX857S release/spotify_releases.csv
-java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=b1249a40-25af-4c72-815e-fbcfeba19839 -did=doi:10.34894/DX857S doc/readme-data.txt
+. credentials.txt
+while true; do
+    read -p "You are about to update the public/derived version of your data. Do you want to continue? Type yes or no." yn
+    case $yn in
+        [Yy]* ) java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=$dataverse_key -did=doi:10.34894/DX857S release/spotify_releases.csv; java -jar DVUploader.jar -server=https://dataverse.nl -ex=.DS_Store -key=$dataverse_key -did=doi:10.34894/DX857S doc/readme-data.txt; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
